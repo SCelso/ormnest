130f7423dfa7af1fad9d741ff2da21af5f40a029
SCelso <sergio.celso.sanchez@gmail.com>
2023-03-08 13:44:42 +0100

    .

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -14,8 +14,7 @@ export class CreateUserDto {
   @MinLength(6)
   @MaxLength(50)
   @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message:
-      'The password must have a Uppercase, lowercase letter and a number',
+    message: 'The password must have a Uppercase, lowercase letter and a numbe',
   })
   password: string;
   @IsString()
